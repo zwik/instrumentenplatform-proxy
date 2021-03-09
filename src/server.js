c0bf4a288f9f7d042ec4427a7f296cd08ea00d00
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2021-03-09 16:31:22 +0100

    Accepting extra query params

--- src/server.js
+++ src/server.js
@@ -3,11 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const inTemp = req.query.intemp;
-  console.log(`binnentemperatuur: ${inTemp}`);
-  res.send('hello world');
+  const { time, intemp, temp, feelslike, dew } = req.query;
+  console.log('time: ', time);
+  console.log('intemp: ', intemp);
+  console.log('temp: ', temp);
+  console.log('feelslike: ', feelslike);
+  console.log('dewpoint: ', dew);
+  res.status(200).json({
+    message: 'OK'
+  });
 });
 
 const server = app.listen(3000, () => {
-  console.log(`Listen on http://${server.address().address}:${server.address().port}`);
+  console.log(`Cumulus proxy istening on port: ${server.address().port}`);
 });
