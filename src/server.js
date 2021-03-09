c2891b5b21105be0060a36ff393a06656911c366
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2021-03-09 16:57:37 +0100

    Added more params

--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ const app = express();
 
 app.get('/', (req, res) => {
   const {
-    time, intemp, temp, feelslike, dew,
+    time, intemp, temp, feelslike, dew, inhum, hum, pressure,
   } = req.query;
   console.log('time: ', new Date(parseInt(time, 10)));
   console.log('intemp: ', intemp);
   console.log('temp: ', temp);
   console.log('feelslike: ', feelslike);
   console.log('dewpoint: ', dew);
+  console.log('inhum: ', inhum);
+  console.log('hum: ', hum);
+  console.log('pressure: ', pressure);
   res.status(200).json({
     message: 'OK',
   });
